311680f7c0a1c874f0e67f6afc1bdf0176fa0643
bigacz <sbigacz@gmail.com>
2026-09-16 02:05:20 +0200

    Add category deletion on frontend

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,10 +3,10 @@ import categoriesController from "../controllers/categoriesController.js";
 
 const categoriesRouter = Router();
 
+categoriesRouter.get("/new", categoriesController.getCategoryNew);
 categoriesRouter.delete("/:categoryId", categoriesController.deleteCategory);
 categoriesRouter.get("/:id", categoriesController.getCategory);
 categoriesRouter.get("/", categoriesController.getAllCategories);
-categoriesRouter.get("/new", categoriesController.getCategoryNew);
 
 categoriesRouter.post("/", categoriesController.postCategories);
 
